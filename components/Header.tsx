870367b48e2fec7efae7cf6b4b41f73033cabd7b
Scott Nelson <snelson@instructors.2u.com>
2023-01-30 12:11:24 -0800

    edited styling for shopping cart button in the Header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ export default function Header({}: Props) {
       <div className="flex-1 flex justify-end">
         <button className="text-3xl relative hover:scale-110">
           <FaShoppingCart />
-          <span className="absolute top-[-8px] right-[-8px] text-base font-bold bg-white border border-red-500 rounded-full text-red-500 w-6 h-6 flex items-center justify-center">2</span>
+          <span className="absolute top-[-10px] right-[-10px] text-base font-bold bg-white border border-red-500 rounded-full text-red-500 w-6 h-6 flex items-center justify-center">2</span>
         </button>
       </div>
     </header>
