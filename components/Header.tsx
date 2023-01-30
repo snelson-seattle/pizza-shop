918a1343bfadf5be5977a83392a51360d81b6a67
Scott Nelson <snelson@instructors.2u.com>
2023-01-29 18:35:55 -0800

    added Header and Footer components and added them to the layout in  _document.tsx

---
+++ components/Header.tsx
@@ -0,0 +1,5 @@
+type Props = {};
+
+export default function Header({}: Props) {
+  return <header>Header</header>;
+}
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <Header />
+
         <Main />
+
+        <Footer />
+        
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
